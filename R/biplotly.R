f4fd52d0124cae8ec9175421d3148b0ff1106ec8
andreagrioni <grioni.andrea@gmail.com>
2021-08-20 14:21:55 +0000

    feat: add interactive biplots and hemolyzed

---
+++ R/biplotly.R
@@ -0,0 +1,44 @@
+#' PCA biplot interactive with plotly
+#'
+#' @param table matrix of aptamer values and metadata
+#' @param x x value
+#' @param y y value
+#' @param color color by feature
+#' @param pca_obj PCAtools::pca object
+#' @return plotly scatterplot of components
+#' @examples
+#' biplotly(t_wdf$df, x="PC1", y="PC2", color="ARM", pca_obj = t_wdf$pca_obj)
+#' @import plotly glue
+#' @export
+biplotly <- function(table, x="PC1", y="PC2", color="ARM", pca_obj=NULL) {
+  x_lab = if (!is.null(pca_obj)) {glue("{x} % variance: {round(pca_obj$variance[[x]], 2)}") } else {glue("{x}")}
+  y_lab = if (!is.null(pca_obj)) {glue("{y} % variance: {round(pca_obj$variance[[y]], 2)}") } else {glue("{y}")}
+  title_ = glue("PCs Comp {x} vs {y}")
+  # draw plot
+  plot_ly(
+    data=table,
+    x = ~.data[[x]],
+    y = ~.data[[y]],
+    type = 'scatter',
+    mode = 'markers',
+    color = ~.data[[color]]
+  ) %>%
+    plotly::add_trace(
+      data=table,
+      hoverinfo = 'text',
+      showlegend = F,
+      text=~paste(
+        '</br> PlateId: ', PlateId,
+        '</br> ScannerID: ', ScannerID,
+        '</br> SampleId: ', SampleId,
+        '</br> ARM: ', ARM,
+        '</br> AGE: ', AGE,
+        '</br> SEX: ', SEX
+      )) %>%
+    plotly::layout(
+      title = glue(title_),
+      xaxis = list(title = glue(x_lab)),
+      yaxis = list(title = glue(y_lab)),
+      legend = list(title=list(text='<b> treatment arm </b>'))
+    )
+}
--- R/hemolyzed_plot.R
+++ R/hemolyzed_plot.R
@@ -4,33 +4,59 @@
 #' @param rm_visual table of filtered by visual
 #' @param rm_hba table of filtered by aptamer value
 #' @param hemo_id aptamer colname
+#' @param interactive draw an interactive plotly
+#' @param title set main title
 #' @return boxplot of aptamer distribution by class
-#' @examples
 #' @importFrom ggplot2 ggplot geom_boxplot
 #' @import dplyr
+#' @import plotly
 #' @export
-hemo_plot <- function(
+hemo_plot <- function (
   accepted,
   rm_visual,
   rm_hba,
-  hemo_id="seq.4915.64"
-  ) {
+  hemo_id="seq.4915.64",
+  interactive=TRUE,
+  title
+  )
+  {
+    target_cols <- c("SampleId", hemo_id)
+    accepted_ <- accepted %>%
+      select(all_of(target_cols)) %>%
+      mutate(flag = "accepted") %>%
+      mutate(status="empty note")
+    rm_visual_ <- rm_visual %>%
+      select(all_of(target_cols)) %>%
+      mutate(flag = "removed") %>%
+      mutate(status="visual check")
+    rm_hba_ <- rm_hba %>%
+      select(all_of(target_cols)) %>%
+      mutate(flag = "removed") %>%
+      mutate(status="hemo check")
 
-  target_cols <- c("SampleId", hemo_id)
-  accepted_ <- accepted %>%
-    select(all_of(target_cols)) %>%
-    mutate(flag="accepted")
-  rm_visual_ <- rm_visual %>%
-    select(all_of(target_cols)) %>%
-    mutate(flag="visual")
-  rm_hba_ <- rm_hba %>%
-    select(all_of(target_cols)) %>%
-    mutate(flag="hemo")
+    tmp_data <- rbind(accepted_, rm_visual_, rm_hba_)
 
-  tmp_data <- rbind(accepted_, rm_visual_, rm_hba_)
+    if (interactive) {
 
-  plot <- tmp_data %>%
-    ggplot(aes(x=flag, y=.data[[hemo_id]], hue=flag)) +
-    geom_boxplot()
-  return(plot)
-}
+      fig <- plot_ly(
+        tmp_data,
+        x = ~flag,
+        y = ~log2(.data[[hemo_id]]),
+        type = "box",
+        marker = list( size = 10),
+        boxpoints = "all",
+        jitter = 0.4,
+        pointpos = 0,
+        hoverinfo = "text",
+        text = ~paste('</br> SampleId: ', SampleId,
+                      '</br> Flag: ', flag,
+                      '</br> Status:', status,
+                      '</br> RFU:', .data[[hemo_id]])) %>%
+        layout(title=title)
+    } else {
+      fig <- tmp_data %>%
+        ggplot(aes(x = flag, y = log2(.data[[hemo_id]]), hue = flag)
+        ) + geom_boxplot() + ggtitle(title)
+    }
+    return(fig)
+  }
